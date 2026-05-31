1eeb6b70a42e18943572d6ebf69b8de2bb16e1c5
areascript <areascript22@gmail.com>
2026-05-31 11:50:55 -0500

    feat: different firebase projects for each flavor

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 val keystorePropertiesFile = rootProject.file("keystore.properties")
@@ -17,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.areascript.passenger_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -33,7 +34,7 @@ android {
         applicationId = "com.areascript.passenger_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
